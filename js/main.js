529c48421a1aa213d875aabe7717b2c43adf4dba
Ivan Severino <ivan.r.severino@gmail.com>
2018-04-05 00:49:41 +0200

    fix semantics on headings. Convert to h2 the headings not found in the page header.

--- js/main.js
+++ js/main.js
@@ -140,11 +140,11 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = "restaurant image";
+  image.alt = 'restaurant image';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
